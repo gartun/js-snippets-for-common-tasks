a2aef2d2993458428494cff7532d0dfdc1a46b1d
gartun <artungokhan96@gmail.com>
2021-05-22 10:47:55 +0300

    finish fixed-header-on-scroll

--- fixed-header-on-scroll/script.js
+++ fixed-header-on-scroll/script.js
@@ -1,11 +1,20 @@
 const header = document.querySelector('header');
 
+// Boolean for avoiding repetitive tasks
+let isReplaced = false;
+
 window.onscroll = () => {
   const winTop = window.pageYOffset;
   
   if(winTop > getHeaderHeight()) {
+    if(!isReplaced) createReplacer();
+    isReplaced = true;
+
     header.classList.add('fixed');
   } else {
+    if(isReplaced) removeReplacer();
+    isReplaced = false;
+
     header.classList.remove('fixed');
   }
   
@@ -15,8 +24,21 @@ function getHeaderHeight() {
   return header.offsetHeight;
 }
 
-function createWrapper() {
-  const wrapper = document.createElement('div');
-  wrapper['aria-hidden'] = 'true';
-  
+// we are creating a replacer to prevent the page from jumping
+// when we fix the header.
+function createReplacer() {
+  const replacer = document.createElement('div');
+
+  // NOT TESTED! added here so that screen-readers don't get confused
+  replacer.setAttribute("aria-hidden", "true");
+
+
+  replacer.classList.add("replacer-div");
+  replacer.style.height = getHeaderHeight() + "px";
+  document.body.insertBefore(replacer, header);
+}
+
+function removeReplacer() {
+  // find the node with the className 'replacer-div' and remove it
+  document.body.removeChild(document.querySelector(".replacer-div"));
 }
