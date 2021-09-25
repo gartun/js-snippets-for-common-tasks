c02aa9cd18c84b1fb8769a4bdda56b7850e6f521
gartun <artungokhan96@gmail.com>
2021-09-25 11:21:15 +0300

    add smooth transition to header

--- fixed-header-on-scroll/script.js
+++ fixed-header-on-scroll/script.js
@@ -1,46 +1,57 @@
-const header = document.querySelector('header');
+const header = document.querySelector("header");
 
 // Boolean for avoiding repetitive tasks
 let isReplaced = false;
 
-window.onscroll = function() {
-  const winTop = window.pageYOffset;
-  if(winTop > getHeaderHeight()) {
-    if(!isReplaced) {
-      createReplacer();
-      isReplaced = true;
-    }
+window.onscroll = function () {
+  setTimeout(() => {
+    if (window.pageYOffset > getHeaderHeight()) {
+      if (!isReplaced) {
+        createReplacer();
 
-    header.classList.add('fixed');
-    header.classList.add('animation-class');
-  } else {
-    if(isReplaced) {
-      removeReplacer();
-      
-      isReplaced = false;
-    }
+        isReplaced = true;
+      }
 
-    header.classList.remove('fixed');
-    header.classList.remove('animation-class');
-  }
-  
-}
+      addClass("fixed");
+      addClass("animation-class");
+    } else {
+      if (isReplaced) {
+        removeReplacer();
+
+        isReplaced = false;
+      }
+
+      remClass("fixed");
+      remClass("animation-class");
+    }
+  }, 150);
+};
 
 function getHeaderHeight() {
   return header.offsetHeight;
 }
 
+function addClass(cls) {
+  const clsList = [].slice.call(header.classList);
+
+  if (clsList.indexOf(cls) === -1) header.classList.add(cls);
+}
+
+function remClass(cls) {
+  header.classList.remove(cls);
+}
+
 // we are creating a replacer to prevent the page from jumping
 // when we fix the header.
 function createReplacer() {
-  const replacer = document.createElement('div');
+  const replacer = document.createElement("div");
 
   // NOT TESTED! added here so that screen-readers don't get confused
   replacer.setAttribute("aria-hidden", "true");
 
   replacer.style.height = getHeaderHeight() + "px";
   replacer.className = "replacer-div";
-  
+
   document.body.insertBefore(replacer, header);
 }
 
